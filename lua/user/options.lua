3cdc36f96c145363b6846bf87a98c37178a81c0f
al-gilang-p <al.gilang.p@gmail.com>
2021-12-23 14:42:19 +0700

    lsp setup

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -31,7 +31,7 @@ local options = {
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
-  guifont = "Hack Nerd Font:h11",               -- the font used in graphical neovim applications
+  guifont = "Hack Nerd Font Mono:h11",               -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append "c"

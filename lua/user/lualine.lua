a4dea2a9f718c52555cf4913250e65b916e4f691
Alwin Gilang Permana <al.gilang.p@bps.go.id>
2021-12-25 07:00:19 +0700

    Modifying lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -45,18 +45,18 @@ local branch = {
 
 local location = {
 	"location",
-	padding = 0,
+	-- padding = 0,
 }
 
 -- cool function for progress
-local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
-end
+--local progress = function()
+	--local current_line = vim.fn.line(".")
+	--local total_lines = vim.fn.line("$")
+	--local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	--local line_ratio = current_line / total_lines
+	--local index = math.ceil(line_ratio * #chars)
+	--return chars[index]
+--end
 
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
@@ -77,8 +77,10 @@ lualine.setup({
 		lualine_c = {},
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_x = { diff, spaces, "encoding", filetype },
+		-- lualine_y = { location },
 		lualine_y = { location },
-		lualine_z = { progress },
+		--lualine_z = { progress },
+		lualine_z = { "progress" },
 	},
 	inactive_sections = {
 		lualine_a = {},
